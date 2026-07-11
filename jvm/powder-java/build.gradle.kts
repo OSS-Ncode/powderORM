d012088aa89abe1971a17d00fbdc1184f1601226
r2adkr <r2adkr05@gmail.com>
2026-07-11 17:35:56 +0900

    fix(release): allow-dirty for staged crate READMEs; node 24 for npm OIDC publish; vanniktech 0.37.0 for current Gradle

--- jvm/powder-java/build.gradle.kts
+++ jvm/powder-java/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
 java {
--- jvm/powder-kotlin/build.gradle.kts
+++ jvm/powder-kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     `java-library`
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
 java {
